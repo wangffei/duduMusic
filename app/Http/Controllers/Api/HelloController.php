7031f75261884ef46639a5be3a5b2e2fa4b923a3
= <2028777058@qq.com>
2020-02-22 22:18:47 +0800

    合并后台页面文件

--- app/Http/Controllers/Api/HelloController.php
+++ app/Http/Controllers/Api/HelloController.php
@@ -53,7 +53,7 @@
    }
 
    public function albums(){
-      $list = DB::select("select id , name , img from albums") ;
+      $list = DB::select("select id , name , img , mid , count from albums") ;
    	$result = Array("code" => 200 , "msg" => "成功" , "data" => ($list)) ;
    	return response(json_encode($result)) -> header("Content-Type" , "application/json") ;
    }
